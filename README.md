403fbde761af71d4a01f61e66a01964c31145f4b
cferdinandi <hello@gomakethings.com>
2016-03-21 14:49:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GMT Events
+Adds an Events custom post type to Go Make Things. [http://gomakethings.com](http://gomakethings.com)
---
+++ gmt-events.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin Name: GMT Events
+ * Plugin URI: https://github.com/cferdinandi/gmt-events/
+ * GitHub Plugin URI: https://github.com/cferdinandi/gmt-events/
+ * Description: Adds an Events custom post type.
+ * Version: 1.0.0
+ * Author: Chris Ferdinandi
+ * Author URI: http://gomakethings.com
+ * License: MIT
+ */
+
+
+// Options & Helpers
+require_once( plugin_dir_path( __FILE__ ) . 'includes/helpers.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'includes/options.php' );
+
+// Custom Post Type
+require_once( plugin_dir_path( __FILE__ ) . 'includes/cpt.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'includes/metabox.php' );
